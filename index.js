8152f2b252b31a61d3b6ca0921d1650c52eb626c
Omurzak Keldibekov <komur.19991008@gmail.com>
2021-11-26 22:39:23 +0600

    update readme

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
